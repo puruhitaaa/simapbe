46bd1c3a3e2df4e1b09c3df7820b9f45f8403826
baiqueee <hughdev101@gmail.com>
2026-01-13 18:11:49 +0700

    fix: auth

--- apps/web/src/middleware/auth.ts
+++ apps/web/src/middleware/auth.ts
@@ -4,9 +4,14 @@ import { authClient } from "@/lib/auth-client";
 
 export const authMiddleware = createMiddleware().server(
   async ({ next, request }) => {
+    const headers = new Headers();
+    const cookie = request.headers.get("cookie");
+    if (cookie) {
+      headers.set("cookie", cookie);
+    }
     const session = await authClient.getSession({
       fetchOptions: {
-        headers: request.headers,
+        headers,
         throw: true,
       },
     });
